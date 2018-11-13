f4f98e2314903e9a067d025de2bea7350eca9d86
Vítězslav Slavík <vitezslavik@gmail.com>
2018-11-13 01:09:33 +0100

    create topic model

---
+++ quora_clone/apps/topics/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from config.settings.base import AUTH_USER_MODEL
+from django.utils.text import slugify
+from django.urls import reverse
+
+
+class Topic(models.Model):
+    """ Group containing posts """
+    name = models.CharField(max_length=75, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    subscribers = models.ManyToManyField(AUTH_USER_MODEL, through='TopicSubscription',
+                                         related_name='topics')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'# {self.name}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Topic, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('topics:topic-detail', kwargs={'topic-slug': self.name})
+
+
+class TopicSubscription(models.Model):
+    """ Bridge Table for Topics <-> User relationship """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('topic', 'user')
+
+    def __str__(self):
+        return f'{self.user} is subscribed to the {self.topic}'
