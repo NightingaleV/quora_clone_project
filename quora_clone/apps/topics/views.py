8b2c4b2df5a045455d235908a577424a154c1fa5
Vítězslav Slavík <vitezslavik@gmail.com>
2018-12-06 13:09:41 +0100

    create topic list view

--- quora_clone/apps/topics/views.py
+++ quora_clone/apps/topics/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from .models import Topic
+
 
 # Create your views here.
 class ListTopic(ListView):
-    pass
+    model = Topic
+    template_name = 'topics/topics_list.html'
