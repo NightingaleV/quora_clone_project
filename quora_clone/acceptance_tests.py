a07f6413fe5efc766d0e09efb73f36a4c5bf4180
Vítězslav Slavík <vitezslavik@gmail.com>
2018-12-06 22:13:12 +0100

    add navbar acceptance test

--- quora_clone/acceptance_tests.py
+++ quora_clone/acceptance_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import unittest
 
 
@@ -12,14 +13,34 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def browset_close_down(self):
-        self.browser.quit()
+    # def test_start_app(self):
+    #     # Goto App Index
+    #     self.browser.get('http://localhost:3000')
+    #     # Check page title
+    #     self.assertIn('Quora', self.browser.title)
+    #     # Check Heading
+    #     try:
+    #         self.browser.find_element_by_tag_name('h1')
+    #     except NoSuchElementException:
+    #         print('Error: Heading 1 not found')
+    #         raise
+    #     # Check Navigation
+    #     try:
+    #         self.browser.find_element_by_tag_name('nav')
+    #     except NoSuchElementException:
+    #         print('Error: Navigation not found')
 
-    def test_start_app(self):
-        # Goto App Index
+    def test_navigation_app(self):
         self.browser.get('http://localhost:3000')
-        # Check page title
-        self.assertIn('Quora', self.browser.title)
+        nav = self.browser.find_element_by_tag_name('nav')
+        menu_items = nav.find_elements_by_class_name('menu-link')
+        for item in menu_items:
+            href = item.get_attribute('href')
+            self.browser.get(href)
+            self.browser.implicitly_wait(2)
+            self.assertNotIn('Page not found', self.browser.title)
+            self.browser.back()
+            print(href)
 
 
 if __name__ == '__main__':
