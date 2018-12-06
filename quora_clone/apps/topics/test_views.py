17210019d1086c27ea4a2fa4c84737ffb9245b3e
Vítězslav Slavík <vitezslavik@gmail.com>
2018-12-06 18:43:34 +0100

    update cbv calling in topics view tests

--- quora_clone/apps/topics/test_views.py
+++ quora_clone/apps/topics/test_views.py
@@ -1,16 +1,31 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 from .views import ListTopic
 
 
-class TopicViews(TestCase):
+def setup_view(view, request, *args, **kwargs):
+    """
+    Mimic ``as_view()``, but returns view instance.
+    Use this function to get view instances on which you can run unit tests,
+    by testing specific methods.
+    """
+
+    view.request = request
+    view.args = args
+    view.kwargs = kwargs
+    return view
+
+
+class TestTopicListView(TestCase):
     def test_topic_url_resolves_to_topic_view(self):
         found = resolve('/topics/')
         self.assertEqual(found.func.view_class, ListTopic)
 
     def test_topic_list_template_implementation(self):
         request = HttpRequest()
-        topic_list = ListTopic()
-        response = topic_list.get(request=request)
+        v = setup_view(ListTopic(), request)
+        response = v.get(request=request).render()
         expected_html = render_to_string('topics/topics_list.html')
         self.assertEqual(response.content.decode(), expected_html)
